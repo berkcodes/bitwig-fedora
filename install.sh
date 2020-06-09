60f1091bcd053536fac4837a3e82b740bac8a8f7
Berk <38805550+berkiyo@users.noreply.github.com>
2020-06-09 16:41:08 +1000

    Added Script Installer
    Just a quick and easy way to install Bitwig Studio without having to clone the whole repo and store it somewhere. This will download into your /tmp directory which won't bloat your system.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo /////         Bitwig Fedora Installer!         /////
+cd /tmp && git clone https://github.com/berkiyo/bitwig-fedora.git && cd bitwig-fedora && chmod +x bitwig-fedora.sh && sudo ./bitwig-fedora.sh -i
